e96f5dbaa5dba8769a6fe3e49b468b48aef68675
Aaviral Agarwal <81667666+aaviralag@users.noreply.github.com>
2022-01-11 23:27:31 +0530

    Create 8.Lucky Division.java

---
+++ CodeForces-A20J/SOLUTION/8.Lucky Division.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if(((n % 4) == 0) || ((n % 7) == 0) || ((n % 44) == 0) || ((n % 47) == 0) | ((n % 74) == 0) || ((n % 444) == 0) || ((n % 447) == 0) || ((n % 474) == 0) || ((n % 477) == 0))	
+		System.out.println("YES");
+	else
+	    System.out.println("NO");
+}}
+    
